20d1955c2d39d019aa7edf6bf5cb6b020d3a0f1a
Maks-Bax <rn4aey@yandex.ru>
2022-08-22 14:48:54 +0300

    greatTestng

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,16 +1,41 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
+//
+//import static org.junit.jupiter.api.Assertions.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.testng.annotations.Test;
 
-class CashbackHackServiceTest {
+import static org.testng.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
 
     @Test
     void shouldRemain() {
-        CashbackHackService service = new CashbackHackService();
-        int expected = 0;
         int actual = service.remain(1000);
-        assertEquals(expected, actual);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void shouldRemainAmount1() {
+        int actual = service.remain(1);
+        int expected = 999;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void shouldLess1000() {
+        int actual = service.remain(800);
+        int expected = 200;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void shouldMore1000() {
+        int actual = service.remain(1300);
+        int expected = 700;
+        assertEquals(actual, expected);
     }
 }
