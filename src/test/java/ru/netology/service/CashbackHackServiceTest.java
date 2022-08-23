b6c870ea9dbc1e54c8633f0d6178a8498b430f7f
Maks-Bax <rn4aey@yandex.ru>
2022-08-23 13:20:30 +0300

    transitionJunit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -4,36 +4,38 @@ package ru.netology.service;
 //
 //import static org.junit.jupiter.api.Assertions.*;
 
-import org.testng.annotations.Test;
+//import org.testng.annotations.Test;
+//
+//import static org.testng.Assert.assertEquals;
 
-import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
-    @Test
-    void shouldRemain() {
+    @org.junit.Test
+    public void shouldRemain() {
         int actual = service.remain(1000);
         int expected = 0;
         assertEquals(actual, expected);
     }
 
-    @Test
-    void shouldRemainAmount1() {
+    @org.junit.Test
+    public void shouldRemainAmount1() {
         int actual = service.remain(1);
         int expected = 999;
         assertEquals(actual, expected);
     }
 
-    @Test
-    void shouldLess1000() {
+    @org.junit.Test
+    public void shouldLess1000() {
         int actual = service.remain(800);
         int expected = 200;
         assertEquals(actual, expected);
     }
 
-    @Test
-    void shouldMore1000() {
+    @org.junit.Test
+    public void shouldMore1000() {
         int actual = service.remain(1300);
         int expected = 700;
         assertEquals(actual, expected);
